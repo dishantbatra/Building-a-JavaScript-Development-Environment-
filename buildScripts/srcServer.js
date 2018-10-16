8c807731301caf013d99d5466f6d1ebc0f17b4eb
DishantBatra <dishant.batra@nagarro.com>
2018-10-16 09:53:22 +0530

    Till Transpiling.

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,22 @@
+//Use for configuring the Server
+// var express = require('express');
+// var path = require('path');
+// var open = require('open'); //open is used to open site on the browser.
+
+import express from 'express';
+import path from 'path';
+import open from 'open' //open is used to open site on the browser.
+
+var port = 3000;
+var app = express();
+app.get('/', function(req,resp){
+resp.sendFile(path.join(__dirname,'../src/index.html'));
+})
+
+app.listen(port,function(err){
+if(err){
+console.log(err);
+}else{
+open('http:localhost:'+port);
+}
+});
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,3 @@
+// var chalk=require('chalk'); es5 syntax
+import chalk from 'chalk'; //es6 syntax
+console.log(chalk.green('Starting the app in dev mode...'));
